a89225e6265e83700640e58993dbfa194934ea30
Peter Hasitschka <peter@hasi.dev>
2022-12-02 07:23:30 +0100

    Pass days in past arguments

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
 import { aggregateReports, printEpics } from './aggregation'
 import ClockifyApi from './api'
+import commandLineArgs from 'command-line-args'
 
 const App = async () => {
+  let daysPast = 0
+  try {
+    const options = commandLineArgs([
+      { name: 'daysPast', alias: 'd', type: Number, defaultValue: 0 },
+    ])
+    daysPast = options.daysPast ?? 0
+  } catch (e) {
+    console.warn('Error on parsing options', e)
+  }
+
   const api = new ClockifyApi()
   await api.initWorkspace()
 
-  const reportLines = await api.getDailyReport(0)
+  const reportLines = await api.getDailyReport(daysPast)
   const aggregatedData = aggregateReports(reportLines)
 
   printEpics(aggregatedData)
